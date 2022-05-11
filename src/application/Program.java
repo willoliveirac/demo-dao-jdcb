eda1a81ca8c6dc71cca6b6caf26198a4f030f5aa
Will Oliveira <will.oliveirac@gmail.com>
2022-05-11 02:09:41 -0300

    Implementação Find All

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,10 +14,15 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: Seller find by Id ===");
+        System.out.println(sellerDao.findById(3));
+
         System.out.println("=== TEST 2: Seller by Department - Solução do Curso ===");
         List<Seller> list = sellerDao.findByDepartment(new Department(2,null));
+        list.forEach(System.out::println);
 
-
+        System.out.println("=== TEST 3: Find All ===");
+        list = sellerDao.findAll();
         list.forEach(System.out::println);
 
 
